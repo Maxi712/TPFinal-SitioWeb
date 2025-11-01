b38a95a40242d99abb0d23cf47aa2b3f93fa0c91
Maximiliano Castro <maxicastro628@gmail.com>
2025-10-31 22:35:26 -0300

    Hice las cosas basicas del form, luego hago el resto

--- Electrodomesticos/jselec.js
+++ Electrodomesticos/jselec.js
@@ -1,4 +1,17 @@
 let link=document.createElement("a")
+
+
 function comprar(){
+    
+    let pro=comprar.closest(".producto")
+    let img=pro.querySelector('.img')
+    let imgform=document.getElementById("producto")
+    localStorage.setItem("imgSrc", img.src)
+    localStorage.setItem("imgAlt",img.alt)
     window.location.href="Comprare.html"
+}
+function p(){
+    document.getElementById("producto")
+    localStorage.getItem("imgSrc")
+    localStorage.getItem("imgAlt")
 }
