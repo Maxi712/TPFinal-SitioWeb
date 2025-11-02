a36aaf88af163e8d82a05ca3e880917b72fd8f67
Maximiliano Castro <maxicastro628@gmail.com>
2025-11-02 00:20:51 -0300

    Hice arreglos en el form y su js
    Mañana hago lo que falta del form

--- Electrodomesticos/jselec.js
+++ Electrodomesticos/jselec.js
@@ -1,17 +1,32 @@
 let link=document.createElement("a")
 
 
-function comprar(){
-    
-    let pro=comprar.closest(".producto")
-    let img=pro.querySelector('.img')
-    let imgform=document.getElementById("producto")
-    localStorage.setItem("imgSrc", img.src)
-    localStorage.setItem("imgAlt",img.alt)
+function comprar(img,desc,marca,pre){
+    localStorage.setItem("proselec", img)
+    localStorage.setItem("descselec", desc)
+    localStorage.setItem("marcaselec", marca)
+    localStorage.setItem("preselec", pre)
     window.location.href="Comprare.html"
 }
-function p(){
-    document.getElementById("producto")
-    localStorage.getItem("imgSrc")
-    localStorage.getItem("imgAlt")
-}
+//aqui es donde voy a cargar la imagen del formulario
+    window.addEventListener('DOMContentLoaded',()=>{
+    let imagen=localStorage.getItem('proselec')
+    let descripcion=localStorage.getItem('descselec')
+    let marca=localStorage.getItem('marcaselec')
+    let precio=localStorage.getItem('preselec')
+    
+    if(imagen){
+        document.getElementById('ima').src=imagen
+    }
+    if(descripcion){
+        document.getElementById('desc').textContent=descripcion
+    }
+    if(marca){
+        document.getElementById('mar').textContent='Marca:'+marca
+    }
+    if(precio){
+        document.getElementById('pre').textContent='Precio:'+precio
+    }
+})
+
+
