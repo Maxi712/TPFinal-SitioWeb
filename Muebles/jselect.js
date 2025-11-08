e27b7908bcf692c9cd0b4530a5a86a5f958672b0
Maximiliano Castro <maxicastro628@gmail.com>
2025-11-07 21:40:12 -0300

    añadi en tecnologia

--- Muebles/jselect.js
+++ Muebles/jselect.js
@@ -1,3 +1,62 @@
+let link=document.createElement("a")
+
+function cancelar(){
+    window.location.href='Muebles.html'
+}
+function comprar(img,desc,marca,pre){
+    localStorage.setItem("proselec", img)
+    localStorage.setItem("descselec", desc)
+    localStorage.setItem("marcaselec", marca)
+    localStorage.setItem("preselec", pre)
+    window.location.href="Compraremue.html"
+}
+//aqui es donde voy a cargar la imagen del formulario
+    window.addEventListener('DOMContentLoaded',()=>{
+    let imagen=localStorage.getItem('proselec')
+    let descripcion=localStorage.getItem('descselec')
+    let marca=localStorage.getItem('marcaselec')
+    let precio=localStorage.getItem('preselec')
+    
+    if(imagen){
+        document.getElementById('ima').src=imagen
+    }
+    if(descripcion){
+        document.getElementById('desc').textContent=descripcion
+    }
+    if(marca){
+        document.getElementById('mar').textContent='Marca:'+marca
+    }
+    if(precio){
+        document.getElementById('pre').textContent='Precio:'+precio
+    }
+})
+//filtros
+function filtrarPor(filtro){
+    let producto=document.querySelectorAll('.producto')
+    for (let i=0;i<producto.length;i++){
+        let marca=producto[i].dataset.marca
+        if(marca===filtro){
+            producto[i].style.display='flex'
+        }else{
+            producto[i].style.display='none'
+        }
+        
+    }
+
+}
+function filtrarPorT(filtro){
+    let producto=document.querySelectorAll('.producto')
+    for (let i=0;i<producto.length;i++){
+        let tipo=producto[i].dataset.tipo
+        if(tipo===filtro){
+            producto[i].style.display='flex'
+        }else{
+            producto[i].style.display='none'
+        }
+        
+    }
+
+}
 function categorias(cat){
     if(cat==='electrodomestico'){
         window.location.href="../Electrodomesticos/Electrodomesticos.html"
@@ -5,3 +64,25 @@ function categorias(cat){
         window.location.href="../Tecnologia/Tecnologia.html"
     }
 }
+//buscador
+const buscador=document.getElementById('buscador')
+document.addEventListener('input',b=>{
+    if (b.target===buscador){
+        const valor= buscador.value.toLowerCase()
+        const producto=document.querySelectorAll('.producto')
+        for (let i=0;i<producto.length;i++){
+        let tipo=producto[i].dataset.tipo.toLowerCase()
+        if(valor===tipo){
+            producto[i].style.display='flex'
+        }else{
+            producto[i].style.display='none'
+        }
+        
+    }
+    }
+})
+
+
+
+
+
--- Tecnologia/jselect.js
+++ Tecnologia/jselect.js
@@ -1,7 +1,64 @@
+function cancelar(){
+    window.location.href='Tecnologia.html'
+}
+function comprar(img,desc,marca,pre){
+    localStorage.setItem("proselec", img)
+    localStorage.setItem("descselec", desc)
+    localStorage.setItem("marcaselec", marca)
+    localStorage.setItem("preselec", pre)
+    window.location.href="Compraretec.html"
+}
+//aqui es donde voy a cargar la imagen del formulario
+    window.addEventListener('DOMContentLoaded',()=>{
+    let imagen=localStorage.getItem('proselec')
+    let descripcion=localStorage.getItem('descselec')
+    let marca=localStorage.getItem('marcaselec')
+    let precio=localStorage.getItem('preselec')
+    
+    if(imagen){
+        document.getElementById('ima').src=imagen
+    }
+    if(descripcion){
+        document.getElementById('desc').textContent=descripcion
+    }
+    if(marca){
+        document.getElementById('mar').textContent='Marca:'+marca
+    }
+    if(precio){
+        document.getElementById('pre').textContent='Precio:'+precio
+    }
+})
+function filtrarPor(filtro){
+    let producto=document.querySelectorAll('.producto')
+    for (let i=0;i<producto.length;i++){
+        let marca=producto[i].dataset.marca
+        if(marca===filtro){
+            producto[i].style.display='flex'
+        }else{
+            producto[i].style.display='none'
+        }
+        
+    }
+
+}
+function filtrarPorT(filtro){
+    let producto=document.querySelectorAll('.producto')
+    for (let i=0;i<producto.length;i++){
+        let tipo=producto[i].dataset.tipo
+        if(tipo===filtro){
+            producto[i].style.display='flex'
+        }else{
+            producto[i].style.display='none'
+        }
+        
+    }
+
+}
+
 function categorias(cat){
     if(cat==='electrodomestico'){
-        window.location.href="../Muebles/Muebles.html"
-    }else if(cat==='tecnologia'){
         window.location.href="../Electrodomesticos/Electrodomesticos.html"
+    }else if(cat==='mueble'){
+        window.location.href="../Muebles/Muebles.html"
     }
 }
