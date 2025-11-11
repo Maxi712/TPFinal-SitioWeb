9916d8eed4aebcbfc6b6323db0cb0b786919efd3
Maximiliano Castro <maxicastro628@gmail.com>
2025-11-10 21:32:28 -0300

    Resolvi los errores y use createElement en el formulario

--- js/forms.js
+++ js/forms.js
@@ -35,14 +35,38 @@ function validarFormulario(event) {
             li.textContent = error;
             listaErrores.appendChild(li);
         });
-        errorContainer.appendChild(listaErrores);
+        errorContainer.appendChild(listaErrores)
         resultadoDiv.appendChild(errorContainer);
         return false;
     } else {
+        const enviadoContainer=document.createElement('div')
+        enviadoContainer.className='enviado-box'
+
         const mensajeExito = document.createElement('p');
         mensajeExito.className = 'success-message';
         mensajeExito.textContent = '¡Formulario enviado con exito!'
-        resultadoDiv.appendChild(mensajeExito);
+        enviadoContainer.appendChild(mensajeExito);
+        resultadoDiv.appendChild(enviadoContainer)
+        const datosContainer =document.createElement('div') 
+        datosContainer.className='datos-cargados' 
+        const correcto=document.createElement('p') 
+        correcto.textContent= `Muy bien ${nombre}, esto son sus datos:` 
+        datosContainer.appendChild(correcto)
+        const listaData=document.createElement('ul')
+        const itemNombre = document.createElement('li')
+        itemNombre.textContent = `Nombre: ${nombre}`
+        listaData.appendChild(itemNombre)
+        const itemGmail = document.createElement('li')
+        itemGmail.textContent = `Gmail: ${email}`
+        listaData.appendChild(itemGmail)
+        const itemTelefono = document.createElement('li')
+        itemTelefono.textContent = `Telefono: ${telefono}`
+        listaData.appendChild(itemTelefono)
+        const itemMensaje = document.createElement('li')
+        itemMensaje.textContent = `Mensaje: ${mensaje}`
+        listaData.appendChild(itemMensaje)
+        datosContainer.appendChild(listaData)
+        resultadoDiv.appendChild(datosContainer)
         document.getElementById('formularioContacto').reset();
         return true;
     }
