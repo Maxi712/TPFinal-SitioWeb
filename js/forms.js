bbb34ea87dac225c07b5515cbe40f494b696ba38
Maximiliano Castro <maxicastro628@gmail.com>
2025-11-12 20:32:29 -0300

    update general

--- js/forms.js
+++ js/forms.js
@@ -50,21 +50,22 @@ function validarFormulario(event) {
         const datosContainer =document.createElement('div') 
         datosContainer.className='datos-cargados' 
         const correcto=document.createElement('p') 
-        correcto.textContent= `Muy bien ${nombre}, esto son sus datos:` 
+        correcto.textContent= 'Muy bien '+ nombre+', esto son sus datos:'
         datosContainer.appendChild(correcto)
         const listaData=document.createElement('ul')
         const itemNombre = document.createElement('li')
-        itemNombre.textContent = `Nombre: ${nombre}`
+        itemNombre.textContent = 'Nombre:'+nombre
         listaData.appendChild(itemNombre)
         const itemGmail = document.createElement('li')
-        itemGmail.textContent = `Gmail: ${email}`
+        itemGmail.textContent = 'Gmail:'+ email
         listaData.appendChild(itemGmail)
         const itemTelefono = document.createElement('li')
-        itemTelefono.textContent = `Telefono: ${telefono}`
+        itemTelefono.textContent = 'Telefono:'+telefono
         listaData.appendChild(itemTelefono)
         const itemMensaje = document.createElement('li')
-        itemMensaje.textContent = `Mensaje: ${mensaje}`
+        itemMensaje.textContent = 'Mensaje:'+ mensaje
         listaData.appendChild(itemMensaje)
+
         datosContainer.appendChild(listaData)
         resultadoDiv.appendChild(datosContainer)
         document.getElementById('formularioContacto').reset();
--- js/select.js
+++ js/select.js
@@ -7,3 +7,13 @@ function categorias(cat){
         window.location.href="Tecnologia/Tecnologia.html"
     }
 }
+
+function categoriascon(cat){
+    if(cat==='mueble'){
+        window.location.href="../Muebles/Muebles.html"
+    }else if(cat==='electrodomestico'){
+        window.location.href="../Electrodomesticos/Electrodomesticos.html"
+    }else if(cat==='tecnologia'){
+        window.location.href="../Tecnologia/Tecnologia.html"
+    }
+}
